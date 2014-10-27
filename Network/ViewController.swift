558f61a78d1b4c14a94b7aaed958cd9714bdb99c
Eduardo Parucker <paruckerr@gmail.com>
2014-10-27 10:38:44 -0200

    Initial Commit

---
+++ Network/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Network
+//
+//  Created by Eduardo Parucker on 27/10/14.
+//  Copyright (c) 2014 Eduardo Parucker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
